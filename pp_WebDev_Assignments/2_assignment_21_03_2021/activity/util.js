c5e60bfa9c069798ecd6c9acebb062815deea18b
iampreetsimar <iampreetsimar@gmail.com>
2021-03-24 18:49:43 +0530

    changed file read/write functionality + added json results files

--- pp_WebDev_Assignments/2_assignment_21_03_2021/activity/util.js
+++ pp_WebDev_Assignments/2_assignment_21_03_2021/activity/util.js
@@ -20,36 +20,23 @@ function writeDataToJsonFile(playerObj, teamName) {
     let playerName = playerObj[0].playerName.replaceAll(" ", "");
     let filePath = path.join(mainDir, teamName, playerName + ".json");
 
-    if(!fs.existsSync(filePath)) {
-        createJsonFile(filePath);
-        fs.writeFile(filePath, JSON.stringify(playerObj, null, 4), function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log(filePath, "was saved"); 
-            }
-        });   
-    } else {
-        let playerArray = readDataFromJsonFile(filePath);
-        playerArray.push(playerObj[0]);
-        fs.writeFile(filePath, JSON.stringify(playerArray, null, 4), function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log(filePath, "was saved"); 
-            }
-        }); 
+    try {
+        // if file  - read data first -> update it -> store it again
+        let contentRead = JSON.parse(fs.readFileSync(filePath));
+        contentRead.push(playerObj[0]);
+        fs.writeFileSync(filePath, JSON.stringify(contentRead, null, 4));
+        console.log(filePath, "was saved");
     }
-}
-
-function createJsonFile(filePath) {
-    let filestream = fs.createWriteStream(filePath);
-    filestream.end();
-    console.log(filePath, "was created");
-}
-
-function readDataFromJsonFile(filePath) {
-    return JSON.parse(fs.readFileSync(filePath));
+    catch (err) {
+        if (err.code === 'ENOENT') {
+            // if file not found - create file and write the data
+            fs.writeFileSync(filePath, JSON.stringify(playerObj, null, 4));
+            console.log(filePath, "was created and saved");
+            return;
+        }
+        console.log(err);
+    }
+    
 }
 
 module.exports = {
