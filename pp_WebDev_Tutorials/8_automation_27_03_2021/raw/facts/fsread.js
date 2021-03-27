02a892051cc839d8aeb5be68023974630b8dcd03
iampreetsimar <iampreetsimar@gmail.com>
2021-03-27 11:11:45 +0530

    initial codebase for automation - promises

---
+++ pp_WebDev_Tutorials/8_automation_27_03_2021/raw/facts/fsread.js
@@ -0,0 +1,46 @@
+let fs = require("fs");
+
+console.log("Before");
+
+// fs.readFile("f1.txt", function cb(err, data) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(data + "");
+//     }
+// })
+
+
+// -----------------------------
+/*
+    Promise is an improvement on callback to do async programming
+    Its return initial state is always pending
+    State becomes fulfilled after the work is complete
+
+    We get a token promise object where state is pending initially
+*/
+
+let promise = fs.promises.readFile("f1.txt", "utf8");
+console.log("Initial State ->", promise);
+console.log("After");
+
+// setTimeout(function() {
+//     console.log("Final State ->", promise);
+// }, 2000);
+
+// setTimeout is not a good way to get value from promises. Maybe the file is large, then it would still return pending
+// We can use a .then() method provided my promise object
+// then is called when promise is fulfilled
+
+// promise.catch() runs if there is some error while processing
+
+promise.then(function(data) {
+    console.log(data);
+})
+
+promise.catch(function(err) {
+    console.log(err);
+})
+
+
+console.log("End");
