a39edd7f93cc8ed1975162348ef193514f34d7c0
iampreetsimar <iampreetsimar@gmail.com>
2021-03-27 23:46:10 +0530

    added functionality to reach editorial tab in warm up challenge 1

--- pp_WebDev_Tutorials/8_automation_27_03_2021/activity/hackerrank_codeSubmit.js
+++ pp_WebDev_Tutorials/8_automation_27_03_2021/activity/hackerrank_codeSubmit.js
@@ -4,7 +4,11 @@ let tab, instance;
 
 console.log("Before");
 
-let browserPromise = puppeteer.launch({ headless: false });
+let browserPromise = puppeteer.launch({ 
+        headless: false,
+        defaultViewport: null,
+        args: ["--start-maximized"]
+    });
 
 browserPromise
     .then(function(browserInstance) {
@@ -19,19 +23,69 @@ browserPromise
         return openLoginPagePromise;
     }).then(function() {
         // type email into input field
+        console.log("...typing email");
         let emailTypedPromise = tab.type("#input-1", email, {delay: 100});
         return emailTypedPromise;
     }).then(function() {
         // type password into input field
+        console.log("...typing password");
         let passwordTypedPromise = tab.type("#input-2", password, {delay: 100});
         return passwordTypedPromise;
     }).then(function() {
         // click on login button
+        console.log("...clicked on login button");
         let loginButtonClickedPromise = tab.click("button[data-analytics='LoginPassword']");
-        return loginButtonClickedPromise;
+        return Promise.all([loginButtonClickedPromise, tab.waitForNavigation({waitUntil: "load"})])
     }).then(function() {
         console.log("Login Successful");
+        console.log("Home Page Opened");
+        return tab.mouse.wheel({ deltaY: 300 });
     }).then(function() {
+        console.log("...clicked on interview prep button");
+        let interviewPrepCardClickedPromise = tab.click("#base-card-1-link", {delay: 200});
+        return Promise.all([
+            interviewPrepCardClickedPromise, 
+            tab.waitForNavigation({waitUntil: "load"}),
+            tab.waitForSelector("a[data-attr1='warmup']")
+        ]);
+    }).then(function() {
+        console.log("Interview Preparation Page Opened");  
+        return tab.mouse.wheel({ deltaY: 200 });
+    }).then(function() {
+        console.log("...clicked on warm up challenges");
+        let warmUpChallengePageClickPromise = tab.click("a[data-attr1='warmup']", {delay: 200});
+        return Promise.all([
+            warmUpChallengePageClickPromise, 
+            tab.waitForNavigation({waitUntil: "load"}), 
+            tab.waitForSelector("a[data-attr1='sock-merchant']")
+        ]);
+    }).then(function() {
+        console.log("Warm Up Challenges Page Opened");
+        return tab.mouse.wheel({ deltaY: 200 });
+    }).then(function() {
+        console.log("...clicked on warm up sort by match challenge");
+        let warmUpChallenge1Promise = tab.click("a[data-attr1='sock-merchant']", {delay: 200});
+        return Promise.all([
+            warmUpChallenge1Promise, 
+            tab.waitForNavigation({waitUntil: "load"}),
+            tab.waitForSelector("div[data-attr2='Editorial']")
+        ]);
+    }).then(function() {
+        console.log("Sales By Match Challenge Page Opened");
+        return tab.mouse.wheel({ deltaY: 200 });
+    }).then(function() {
+        console.log("...clicked on sort by match challenge editorial tab");
+        let salesByMatchChallengeEditorialClickedPromise = tab.click("div[data-attr2='Editorial']", {delay: 200});
+        return Promise.all([
+            salesByMatchChallengeEditorialClickedPromise, 
+            tab.waitForNavigation({waitUntil: "load"}),
+            tab.waitForSelector("button[class='ui-btn ui-btn-normal ui-btn-primary ui-btn-styled']")
+        ]);
+    }).then(function() {
+        console.log("Sales By Match Challenge Editorial Opened");
+        return tab.mouse.wheel({ deltaY: 500 });
+    })
+    .then(function() {
         setTimeout(function() {
             // close browser instance
             instance.close();
