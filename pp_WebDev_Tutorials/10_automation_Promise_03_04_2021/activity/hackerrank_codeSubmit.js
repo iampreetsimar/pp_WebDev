d8e3cc251667d84e71db1bdb6018b5cb177b2905
iampreetsimar <iampreetsimar@gmail.com>
2021-04-03 16:32:42 +0530

    functionality to submit first code added

--- pp_WebDev_Tutorials/10_automation_Promise_03_04_2021/activity/hackerrank_codeSubmit.js
+++ pp_WebDev_Tutorials/10_automation_Promise_03_04_2021/activity/hackerrank_codeSubmit.js
@@ -62,10 +62,6 @@ browserPromise
         return warmUpChallengePageClickPromise;
     }).then(function() {
         console.log("Warm Up Challenges Page Opened,", "...clicking on individual warm up challenge questions");
-
-        // let warmUpChallengePromise = waitAndClick("a[data-attr1='sock-merchant']");
-        // return warmUpChallengePromise;
-
         // returns current module page url
         return tab.url();
     }).then(function(url) {
@@ -125,8 +121,50 @@ function codeSubmitter(modulePageURL, code, questionName) {
                     questionElement[idx].click();
                 };
 
+                // evaluate takes the function to be run in console + arguments to pass to the said function
                 let challengePageClickPromise = tab.evaluate(browserConsoleFunction, questionName);
                 return challengePageClickPromise;
+            })
+            
+            // this part of code will run fine in Windows but not on MAC - Any keyboard event with Ctrl will not work
+            .then(function() {
+                // click on checkbox
+                let inputWillBeClickedPromise = waitAndClick(".custom-checkbox.inline");
+                return inputWillBeClickedPromise;
+            }).then(function () {
+                // type the code 
+                let codeWillBeTypedPromise = tab.type(".custominput", code);
+                return codeWillBeTypedPromise;
+            }).then(function () {
+                // hold down Control
+                let holdControlDownPromise = tab.keyboard.down("Control");
+                return holdControlDownPromise;
+            }).then(function () {
+                // Press ctrl a
+                let pressApromise = tab.keyboard.press("a");
+                return pressApromise;
+            }).then(function () {
+                // Press ctrl x to cut code typed in input
+                let cutPromise = tab.keyboard.press("x");
+                return cutPromise;
+            }).then(function () {
+                // editor will be clicked
+                let editorWillBeClickedPromise = tab.click(".monaco-editor.no-user-select.vs");
+                return editorWillBeClickedPromise;
+            }).then(function () {
+                // press ctrl a to select all code already present
+                let aisPressedpromise = gtab.keyboard.press("a");
+                return aisPressedpromise;
+            })
+            .then(function () {
+                // press ctrl v to paste the code selected from input
+                let pastePromise = gtab.keyboard.press("v");
+                return pastePromise;
+            })
+            .then(function () {
+                // click submit code button
+                let submitIsClickedPromise = gtab.click(".pull-right.btn.btn-primary.hr-monaco-submit");
+                return submitIsClickedPromise;
             }).then(function() {
                 resolve();
             }).catch(function() {
@@ -135,3 +173,28 @@ function codeSubmitter(modulePageURL, code, questionName) {
     });
 }
 
+
+// // CUSTOM PROMISE TO SET AUTOCOMPLETE SETTINGS TO DISABLE IN CODE EDITOT
+// function autoCompleteSettingsHanlder() {
+//     return new Promise(function(resolve, reject) {
+
+//         // wait for settings selector and click
+//         let settingsClickedPromise = waitAndClick("button[aria-label='Editor Settings']");
+//         settingsClickedPromise
+//             .then(function() {
+//                 // wait to disable button and click
+//                 let autoCompleteDisablePromise = waitAndClick("button[aria-label='Disable Autocomplete']");
+//                 return autoCompleteDisablePromise;
+//             }).then(function() {
+//                 // click inside editor/settings selector
+//                 let settingsHidePromise = click("button[aria-label='Editor Settings']");
+//                 return settingsHidePromise;
+//             }).then(function() {
+//                 resolve();
+//             }).catch(function() {
+//                 reject();
+//             })
+        
+
+//     })
+// }
