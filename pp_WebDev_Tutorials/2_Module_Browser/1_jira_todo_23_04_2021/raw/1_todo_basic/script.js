9caf54340d7bc9ffaaa69db5e36d9b0dfeaed8b5
iampreetsimar <iampreetsimar@gmail.com>
2021-04-23 22:40:50 +0530

    added css and js for basic to-do app

--- pp_WebDev_Tutorials/2_Module_Browser/1_jira_todo_23_04_2021/raw/1_todo_basic/script.js
+++ pp_WebDev_Tutorials/2_Module_Browser/1_jira_todo_23_04_2021/raw/1_todo_basic/script.js
@@ -1,6 +1,8 @@
 let inputElement = document.querySelector(".user-input");
 let taskList = document.querySelector(".task-list");
 
+inputElement.focus();
+
 // keypress event on input element
 inputElement.addEventListener("keypress", function(e) {
     // browser gives an event 'e' object when an event occurs
@@ -17,12 +19,20 @@ inputElement.addEventListener("keypress", function(e) {
         // add class to the element
         newTask.setAttribute("class", "task");
 
+        // adding event listener on the new element
+        newTask.addEventListener("dblclick", function() {
+            // delete the element
+            this.remove();
+            console.log('"' + this.innerText + '"', "was removed from the to-do list");
+            inputElement.focus();
+        });
+
         // append this element to this parent - adds to the last
         taskList.appendChild(newTask);
 
         // setting input element value to empty
         inputElement.value = "";
         
-        console.log(taskName, "was added to the to-do list");
+        console.log('"' + taskName + '"' , "was added to the to-do list");
     }
-})
+});
