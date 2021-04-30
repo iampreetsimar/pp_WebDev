4a1db8d8f4968f3dccdb25820635a9493e13556e
iampreetsimar <iampreetsimar@gmail.com>
2021-04-30 22:51:00 +0530

    added to delete tasks + added localStorage for add, remove and editing mode

--- pp_WebDev_Tutorials/2_Module_Browser/4_jira_todo_30_04_2021/activity/script.js
+++ pp_WebDev_Tutorials/2_Module_Browser/4_jira_todo_30_04_2021/activity/script.js
@@ -2,10 +2,13 @@ let filterColorButton = document.querySelectorAll(".filter-color");
 let mainContainer = document.querySelector(".main-container");
 let filter = document.querySelectorAll(".filter");
 let body = document.body;
-let addTask = document.querySelector(".fa-plus");
-let deleteTask = document.querySelector(".fa-trash-alt");
+let addTaskButton = document.querySelector(".fa-plus");
+let deleteTaskButton = document.querySelector(".fa-trash-alt");
 let modes = ["new", "active", "resolved", "failed"];
+let deleteState = false;
+let uid = new ShortUniqueId();
 
+let tasklistArray = [];
 // for(let i = 0; i < filterColorButton.length; i++) {
 //     filterColorButton[i].addEventListener("click", function(e) {
 //         // to fetch class added to the button
@@ -22,13 +25,31 @@ let modes = ["new", "active", "resolved", "failed"];
 //     })
 // }
 
-addTask.addEventListener("click", createModal);
+// add tasks from localStorage
+if(localStorage.getItem("todoApp")) {
+    tasklistArray = JSON.parse(localStorage.getItem("todoApp"));
+    for(let i = 0; i < tasklistArray.length; i++) {
+        let taskObject = tasklistArray[i];
+        createTask(taskObject.description, taskObject.mode, taskObject.id, false);
+    }
+}
 
-function createModal() {
+addTaskButton.addEventListener("click", createModal);
+deleteTaskButton.addEventListener("click", toggleDeleteState);
+
+function createModal(e) {
+    let existingModal = document.querySelector(".modal-container");
+    if(existingModal) {
+        let taskInput = existingModal.querySelector(".modal-input");
+        taskInput.value = "";
+        taskInput.focus();
+        return;
+    }
+        
     let modalContainer = document.createElement("div");
-    let actionsList = document.querySelectorAll(".actions");
-    let addButton = actionsList[0];
-    addButton.style.backgroundColor = "darkslategrey";
+    let addButton = e.currentTarget;
+    let parendAddButton = addButton.parentNode;
+    parendAddButton.classList.add("active-icon");
     modalContainer.setAttribute("class", "modal-container");
     modalContainer.innerHTML = 
         `
@@ -44,10 +65,10 @@ function createModal() {
         `;
 
     body.appendChild(modalContainer);
-    handleModal(modalContainer, addButton);
+    handleModal(modalContainer, parendAddButton);
 }
 
-function handleModal(modalContainer, addButton) {
+function handleModal(modalContainer, parendAddButton) {
     let modalInput = document.querySelector(".modal-input");
     modalInput.focus();
 
@@ -75,7 +96,7 @@ function handleModal(modalContainer, addButton) {
         if(e.key == "Enter") {
             if(modalInput.value == "") {
                 modalContainer.remove();
-                addButton.style.backgroundColor = "rgb(73, 107, 107)";
+                parendAddButton.classList.remove("active-icon");
                 return;
             }
 
@@ -83,27 +104,41 @@ function handleModal(modalContainer, addButton) {
             console.log('"' + todoItem + '"', "was added in the to-do list.");
 
             modalContainer.remove();
-            addButton.style.backgroundColor = "rgb(73, 107, 107)";
-            createTask(todoItem, currentSelectedMode);
+            parendAddButton.classList.remove("active-icon");
+            createTask(todoItem, currentSelectedMode, uid(), true);
         }
     });
 };
 
-function createTask(todoItem, currentSelectedMode) {
+function createTask(todoItem, currentSelectedMode, taskId, fromUI) {
     let taskContainer = document.createElement("div");
     taskContainer.setAttribute("class", "task-container");
     taskContainer.innerHTML =
         `
             <div class="task-filter ${currentSelectedMode}"></div>
             <div class="task-description-container">
-                <h3 class="task-id">#example</h3>
+                <h3 class="task-id">#${taskId}</h3>
                 <div class="task-description" contenteditable="true">${todoItem}</div>
             </div>
         `;
     
     mainContainer.prepend(taskContainer);
+
+    // add new item to localStorage
+    if(fromUI) {
+        let taskObject = {
+            id: taskId,
+            description: todoItem,
+            mode: currentSelectedMode
+        };
+    
+        tasklistArray.push(taskObject);
+        localStorage.setItem("todoApp", JSON.stringify(tasklistArray));
+    }
+
     let taskFilter = taskContainer.querySelector(".task-filter");
     taskFilter.addEventListener("click", changeTaskMode);
+    taskContainer.addEventListener("click", deleteTask);
 };
 
 function changeTaskMode(e) {
@@ -114,4 +149,60 @@ function changeTaskMode(e) {
     let idxCurrentMode = modes.indexOf(currentMode);
     taskFilter.classList.remove(currentMode);
     taskFilter.classList.add(modes[(idxCurrentMode + 1) % modes.length]);
+
+    // update localStorage
+    let taskId = taskFilter.parentNode.querySelector(".task-id").innerText.substring(1);
+    for(let i = 0; i < tasklistArray.length; i++) {
+        if(tasklistArray[i].id == taskId) {
+            tasklistArray[i].mode = modes[(idxCurrentMode + 1) % modes.length];
+            break;
+        }
+    }
+    localStorage.setItem("todoApp", JSON.stringify(tasklistArray));
 };
+
+function toggleDeleteState(e) {
+    let todoItems = mainContainer.querySelector(".task-container");
+    if(!todoItems) {
+        console.log("No tasks added. To-Do list is empty.");
+        return;
+    }
+
+    let deleteButton = e.currentTarget;
+    let parent = deleteButton.parentNode;
+    if(deleteState) {
+        parent.classList.remove("active-icon")
+    } else {
+        parent.classList.add("active-icon");
+    }
+
+    deleteState = !deleteState;
+}
+
+function deleteTask(e) {
+    if(deleteState) {
+        let item = e.currentTarget;
+        let currentTask = item.querySelector(".task-description").innerText;
+
+        // update localStorage
+        let taskId = item.querySelector(".task-id").innerText.substring(1);
+        for(let i = 0; i < tasklistArray.length; i++) {
+            if(tasklistArray[i].id == taskId) {
+                tasklistArray.splice(i, 1);
+                break;
+            }
+        }
+        localStorage.setItem("todoApp", JSON.stringify(tasklistArray));
+
+        console.log('"' + currentTask + '"', "was removed from the to-do list.");
+        item.remove();
+
+        let todoItems = mainContainer.querySelector(".task-container");
+        if(!todoItems) {
+            let deleteButton = document.querySelector(".fa-trash-alt");
+            let parent = deleteButton.parentNode;
+            parent.classList.remove("active-icon");
+            deleteState = false;
+        }
+    }
+}
