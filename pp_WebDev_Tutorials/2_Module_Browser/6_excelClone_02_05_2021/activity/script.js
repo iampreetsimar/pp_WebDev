e81bafb7bc827a18cf524e7c92d6df4443667f96
iampreetsimar <iampreetsimar@gmail.com>
2021-05-03 04:04:58 +0530

    initial codebase for 2nd May, 2021

---
+++ pp_WebDev_Tutorials/2_Module_Browser/6_excelClone_02_05_2021/activity/script.js
@@ -0,0 +1,35 @@
+let addSheet = document.querySelector(".fa-plus");
+let sheetlist = document.querySelector(".sheet-list");
+let currentSheetIdx = 0;
+
+// default sheet - event listener to make it active on load
+sheetlist.children[0].addEventListener("click", selectSheet);
+
+// adds new sheet
+addSheet.addEventListener("click", function() {
+    let totalSheets = document.querySelectorAll(".sheet");
+    let lastIdx = totalSheets.length;
+    let newSheet = document.createElement("div");
+    newSheet.setAttribute("class", "sheet active-sheet");
+    newSheet.setAttribute("sheetIdx", lastIdx);
+    newSheet.innerText = `Sheet ${lastIdx + 1}`;
+    totalSheets[currentSheetIdx].classList.remove("active-sheet");
+    currentSheetIdx = lastIdx;
+    newSheet.addEventListener("click", selectSheet);
+    sheetlist.append(newSheet);
+});
+
+// makes selected sheet as active
+function selectSheet(e) {
+    let selectedSheet = e.currentTarget;
+
+    // if selected sheed is already selected
+    if(selectedSheet.classList[1])
+        return;
+    
+    let totalSheets = document.querySelectorAll(".sheet");
+    totalSheets[currentSheetIdx].classList.remove("active-sheet");
+    currentSheetIdx = Number(selectedSheet.getAttribute("sheetIdx"));
+    selectedSheet.classList.add("active-sheet");
+}
+
