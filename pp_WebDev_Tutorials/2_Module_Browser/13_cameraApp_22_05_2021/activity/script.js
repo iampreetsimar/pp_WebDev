76b2112b890b2e2f5eb285a9bddd266f963fa78e
iampreetsimar <iampreetsimar@gmail.com>
2021-05-23 05:29:44 +0530

    added changes for button animation + video timer + UI changes for filters + UI changes for zoom in/out

--- pp_WebDev_Tutorials/2_Module_Browser/13_cameraApp_22_05_2021/activity/script.js
+++ pp_WebDev_Tutorials/2_Module_Browser/13_cameraApp_22_05_2021/activity/script.js
@@ -1,11 +1,13 @@
 let videoEl = document.querySelector("#video-stream");
 let recordBtn = document.querySelector("#record-btn");
 let captureBtn = document.querySelector("#capture-btn");
+let recordTime = document.querySelector(".record-time-container");
 
 let buffer = [];
 let constraints = { video: true, audio: true };
 let mediaRecorder;
 let recordState = false;
+let clearTimerObject;
 
 navigator.mediaDevices.getUserMedia(constraints)
 .then(function (mediaStream) {
@@ -32,17 +34,40 @@ navigator.mediaDevices.getUserMedia(constraints)
 
 recordBtn.addEventListener("click", function () {
     if(!recordState) {
-        recordBtn.innerText = "Recording..";
+        recordBtn.classList.add("record-animation");
         mediaRecorder.start();
+        startTimer();
         recordState = true;
     } else {
-        recordBtn.innerText = "Record";
+        recordBtn.classList.remove("record-animation");
         mediaRecorder.stop();
+        stopTimer();
         recordState = false;
     }
 })
 
+function startTimer() {
+    let timerCount = 0;
+    recordTime.classList.add("timing-active");
+    clearTimerObject = setInterval(function () {
+        let seconds = timerCount % 60 < 10 ? `0${timerCount % 60}` : `${timerCount % 60}`;
+        let minutes = Number.parseInt(timerCount / 60) < 10 ? `0${Number.parseInt(timerCount / 60)}` : `${Number.parseInt(timerCount / 60)}`;
+        let hours = Number.parseInt(timerCount / 3600) < 10 ? `0${Number.parseInt(timerCount / 3600)}` : `${Number.parseInt(timerCount / 3600)}`;
+        recordTime.innerText = `${hours}:${minutes}:${seconds}`;
+        timerCount++;
+    }, 1000);
+}
+
+function stopTimer() {
+    recordTime.classList.remove("timing-active");
+    recordTime.innerText = "00:00:00";
+    clearInterval(clearTimerObject);
+}
+
 captureBtn.addEventListener("click", function () {
+    // added capture animation class
+    captureBtn.classList.add("capture-animation");
+
     // create a canvas
     // set height and width equal to video frame
     let canvas = document.createElement("canvas");
@@ -64,4 +89,11 @@ captureBtn.addEventListener("click", function () {
     a.click();
     a.remove();
     canvas.remove();
-})
+
+    // animation should not be removed abrubtly
+    setTimeout(function () {
+        captureBtn.classList.remove("capture-animation");
+    }, 400);
+});
+
+
