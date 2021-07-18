78aa20a44b4902efe3e53996383809e8f8b3e026
iampreetsimar <iampreetsimar@gmail.com>
2021-07-18 23:25:00 +0530

    added poc for material UI dialogs

--- pp_WebDev_Tutorials/3_Module_ReactJS/11_reelsApp_10_07_2021/activity/reels-app/src/MaterialUI/Cards.js
+++ pp_WebDev_Tutorials/3_Module_ReactJS/11_reelsApp_10_07_2021/activity/reels-app/src/MaterialUI/Cards.js
@@ -10,6 +10,12 @@ import Typography from '@material-ui/core/Typography';
 import vid from "./vid.mp4";
 import style from "./Cards.module.css";
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 const useStyles = makeStyles({
   root: {
     margin: "0 auto",
@@ -22,6 +28,16 @@ const useStyles = makeStyles({
 
 export default function Cards() {
   const classes = useStyles();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClickClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Card className={classes.root}>
@@ -41,9 +57,26 @@ export default function Cards() {
         <Button size="small" color="primary">
           Share
         </Button>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={handleClickOpen}>
           Learn More
         </Button>
+        <Dialog open={open} onClose={handleClickClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClickClose} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={handleClickClose} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
       </CardActions>
     </Card>
   );
