d735de179be0a89c350e09a91a1aa741d85b7d26
iampreetsimar <iampreetsimar@gmail.com>
2021-07-18 23:08:59 +0530

    added MaterialUI poc for navbar, cards and buttons

--- pp_WebDev_Tutorials/3_Module_ReactJS/11_reelsApp_10_07_2021/activity/reels-app/src/App.js
+++ pp_WebDev_Tutorials/3_Module_ReactJS/11_reelsApp_10_07_2021/activity/reels-app/src/App.js
@@ -1,12 +1,16 @@
 import AuthProvider from "./Context/AuthProvider";
 import Signup from "./Components/Signup";
+import Main from "./MaterialUI/Main";
 
 function App() {
   return (
     // signup is passed as props.children to AuthProvider
-    <AuthProvider>
-      <Signup />
-    </AuthProvider>
+    // <AuthProvider>
+    //   <Signup />
+    // </AuthProvider>
+
+    // Material UI POC
+    <Main />
   );
 }
 
